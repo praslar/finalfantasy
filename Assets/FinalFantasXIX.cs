c9f39aaeb9bf16ea44a81bc1237cbc1bda0c80b8
Thang Pham <thang.pham@jamalex.io>
2021-05-15 19:06:57 +0700

    FinalFantasyXIX

---
+++ Assets/FinalFantasXIX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalFantasXIX : MonoBehaviour
+{
+    [SerializeField] Text storyText;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        storyText.text = state.GetStateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
